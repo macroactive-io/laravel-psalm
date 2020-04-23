e68149bd769e348aad4ba8c040d28cb0f861fd0e
Aleksandr Zhuravlev <aleks@macroactive.com>
2020-04-24 10:14:58 +1200

    Apparently Collections did not have ArrayAccess and Iterator properties. Fixed

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,8 +13,8 @@ final class Plugin implements PluginEntryPointInterface
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
     {
         $dir = dirname(__DIR__);
-        $psalm->addStubFile($dir . '/stubs/Collection.php');
-        $psalm->addStubFile($dir . '/stubs/Model.php');
-        $psalm->addStubFile($dir . '/stubs/functions.php');
+        $psalm->addStubFile($dir . '/stubs/Enumerable.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Collection.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Model.stubphp');
     }
 }
