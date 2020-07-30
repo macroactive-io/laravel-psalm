ceac67b526ab549c83429641ca2fd01d4a0d2d25
Aleksandr Zhuravlev <aleks@macroactive.com>
2020-07-30 19:04:03 +1200

    Added relations and few work around it

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,11 +13,16 @@ final class Plugin implements PluginEntryPointInterface
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
     {
         $dir = dirname(__DIR__);
-        $psalm->addStubFile($dir . '/stubs/Enumerable.stubphp');
+        $psalm->addStubFile($dir . '/stubs/BelongsTo.stubphp');
+        $psalm->addStubFile($dir . '/stubs/BelongsToMany.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Builder.stubphp');
         $psalm->addStubFile($dir . '/stubs/Collection.stubphp');
         $psalm->addStubFile($dir . '/stubs/EloquentCollection.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Enumerable.stubphp');
+        $psalm->addStubFile($dir . '/stubs/HasMany.stubphp');
+        $psalm->addStubFile($dir . '/stubs/HasOne.stubphp');
+        $psalm->addStubFile($dir . '/stubs/HasRelationships.stubphp');
         $psalm->addStubFile($dir . '/stubs/Model.stubphp');
-        $psalm->addStubFile($dir . '/stubs/Builder.stubphp');
         $psalm->addStubFile($dir . '/stubs/Paginator.stubphp');
     }
 }
