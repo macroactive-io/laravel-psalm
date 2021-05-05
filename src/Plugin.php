4613ca6344af7187f53dd812ffbe19df56777e5c
Aleksandr Zhuravlev <aleks@macroactive.com>
2021-05-05 23:15:58 +1200

    Stub improvements. Laravel forces to copypaste :/

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,13 +13,15 @@ final class Plugin implements PluginEntryPointInterface
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
     {
         $dir = dirname(__DIR__);
+        $psalm->addStubFile($dir . '/stubs/Relation.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Builder.stubphp');
         $psalm->addStubFile($dir . '/stubs/BelongsTo.stubphp');
         $psalm->addStubFile($dir . '/stubs/BelongsToMany.stubphp');
-        $psalm->addStubFile($dir . '/stubs/Builder.stubphp');
         $psalm->addStubFile($dir . '/stubs/Collection.stubphp');
         $psalm->addStubFile($dir . '/stubs/Contracts.stubphp');
         $psalm->addStubFile($dir . '/stubs/EloquentCollection.stubphp');
         $psalm->addStubFile($dir . '/stubs/Enumerable.stubphp');
+        $psalm->addStubFile($dir . '/stubs/HasOneOrMany.stubphp');
         $psalm->addStubFile($dir . '/stubs/HasMany.stubphp');
         $psalm->addStubFile($dir . '/stubs/HasOne.stubphp');
         $psalm->addStubFile($dir . '/stubs/HasRelationships.stubphp');
