27937cec68a0df92a725b5ea50814207b0011021
Aleksandr Zhuravlev <aleks@macroactive.com>
2021-05-31 20:39:33 +1200

    Added a stub for console command

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,6 +19,7 @@ final class Plugin implements PluginEntryPointInterface
         $psalm->addStubFile($dir . '/stubs/BelongsToMany.stubphp');
         $psalm->addStubFile($dir . '/stubs/Collection.stubphp');
         $psalm->addStubFile($dir . '/stubs/Contracts.stubphp');
+        $psalm->addStubFile($dir . '/stubs/Console.stubphp');
         $psalm->addStubFile($dir . '/stubs/EloquentCollection.stubphp');
         $psalm->addStubFile($dir . '/stubs/Enumerable.stubphp');
         $psalm->addStubFile($dir . '/stubs/HasOneOrMany.stubphp');
