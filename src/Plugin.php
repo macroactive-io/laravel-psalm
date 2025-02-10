f6068ab634689c670991a3b94e33ed61cd0e6a90
Aleksandr Zhuravlev <scif-1986@ya.ru>
2025-02-10 12:32:57 +1000

    Initial Laravel 11 support and Pslam 6

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,17 +13,12 @@ final class Plugin implements PluginEntryPointInterface
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
     {
         $dir = dirname(__DIR__);
-        $psalm->addStubFile($dir . '/stubs/Relation.stubphp');
         $psalm->addStubFile($dir . '/stubs/Builder.stubphp');
-        $psalm->addStubFile($dir . '/stubs/BelongsTo.stubphp');
-        $psalm->addStubFile($dir . '/stubs/BelongsToMany.stubphp');
         $psalm->addStubFile($dir . '/stubs/Collection.stubphp');
         $psalm->addStubFile($dir . '/stubs/Contracts.stubphp');
         $psalm->addStubFile($dir . '/stubs/Console.stubphp');
-        $psalm->addStubFile($dir . '/stubs/HasOneOrMany.stubphp');
-        $psalm->addStubFile($dir . '/stubs/HasMany.stubphp');
-        $psalm->addStubFile($dir . '/stubs/HasOne.stubphp');
         $psalm->addStubFile($dir . '/stubs/HasRelationships.stubphp');
+
         $psalm->addStubFile($dir . '/stubs/Model.stubphp');
         $psalm->addStubFile($dir . '/stubs/Paginator.stubphp');
         $psalm->addStubFile($dir . '/stubs/Providers.stubphp');
